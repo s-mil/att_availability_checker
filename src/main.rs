776b35bf8cddb35d005fcf50d26580a1d3047756
Sam Miller <sithpow@gmail.com>
2023-01-25 00:16:07 -0600

    switch to clap for args

--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,35 @@
-use std::env;
 use reqwest;
 use serde_json::json;
 use serde_json::Value;
+use clap::Parser;
 
+
+#[derive(Parser, Debug)]
+#[command(author, version, about, long_about = None)]
+struct Args {
+
+    /// Zip code of address
+
+    zip: Option<String>,
+
+    /// Street address "123 main"
+  
+    street: Option<String>,
+
+
+}
 
 #[cfg(not(target_arch = "wasm32"))]
 #[tokio::main]
 async fn main()-> Result<(), reqwest::Error> {
- 
+    
 
-    let args: Vec<String> = env::args().collect();
+    let args = Args::parse();
 
     let echo_json: Value = reqwest::Client::new()
         .get("https://www.att.com/services/shop/model/ecom/shop/view/unified/qualification/service/CheckAvailabilityRESTService/invokeCheckAvailability")
-        .query(&[("userInputZip",args[1].to_string()), 
-                ("userInputAddressLine1", args[2].to_string()),
+        .query(&[("userInputZip", args.zip.expect("need zip")), 
+                ("userInputAddressLine1", args.street.expect("Need street address")),
                 ("mode", "fullAddress".to_string())])
         .header("Accept", "*/*")
         .header("Connection", "keep-alive")
@@ -24,11 +39,11 @@ async fn main()-> Result<(), reqwest::Error> {
         .json()
         .await?;
 
-    println!("{:?}", json!(echo_json).get("profile").expect("Not found").get("isGIGAFiberAvailable").unwrap().as_bool().unwrap()
-);
+    let fiber: bool = json!(echo_json).get("profile").expect("Profile Not found").get("isGIGAFiberAvailable").expect("didnt find the GigaFiber").as_bool().expect("It didnt parse the json right");
+    println!("{}", &fiber);
 
 
-    Ok(())
+    return Ok(());
 }
 
 #[cfg(target_arch = "wasm32")]
