dfd76306733158df57ac9f56a4b1f75ae0f10aed
Sam Miller <23262411+s-mil@users.noreply.github.com>
2023-11-09 21:15:56 -0600

    Add raw (#3) (#4)
    * update cargo dependencies
    
    * redirect readme links
    
    * add nix shell config
    
    * add raw function
    
    * update cargo lock

---
+++ shell.nix
@@ -0,0 +1,18 @@
+let
+  mozilla = import (builtins.fetchTarball https://github.com/mozilla/nixpkgs-mozilla/archive/master.tar.gz);
+  nixpkgs = import <nixpkgs> { overlays = [ mozilla ]; };
+in
+
+  with nixpkgs;
+
+  mkShell {
+    buildInputs = [
+      clang # needed for bindgen
+      latest.rustChannels.stable.rust
+      openssl
+      pkgconfig # needed for libdbus-sys to find the paths
+      dbus.dev
+    ];
+
+    LIBCLANG_PATH = "${llvmPackages.libclang}/lib";
+  }
